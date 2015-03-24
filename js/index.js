965b4ac6dc69383b4012ec95e320e3b5852d1334
nonittas <noura1991@stu.aii.edu>
2015-03-24 06:44:32 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $('document').ready(function() {
-	var options = { videoId: 'e4Is32W-ppk', start: 3 };
+	var options = { videoId: 'P2x3-b6JEj8', start: 3 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });
+
